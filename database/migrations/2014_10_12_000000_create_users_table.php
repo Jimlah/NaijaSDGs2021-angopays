d0f11ac335bbf87f89038836b25e43f2abd465a2
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-03-26 21:32:41 +0100

    patch: added phone_number to authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('phone_number')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['superadmin', 'admin', 'user'])->default('user');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
